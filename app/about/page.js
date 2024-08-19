6c10259ea5cb483211c47f9f294e18fcab29b096
Aniekan210 <ani8dar@gmail.com>
2024-08-19 15:45:00 +0100

    Responsive Hook

---
+++ app/about/page.js
@@ -0,0 +1,15 @@
+import { useResponsiveJSX } from '@/app/useResponsiveJSX.js';
+
+const page = () => {
+    const breakpoint = useResponsiveJSX([600,900,1200]);
+    return (
+        <>
+            {breakpoint === 0 && <div>Mobile View</div>}
+            {breakpoint === 1 && <div>Tablet View</div>}
+            {breakpoint === 2 && <div>Desktop View</div>}
+            {breakpoint === 3 && <div>Large Desktop View</div>}
+        </>
+    )
+}
+
+export default page
