dd3e7a7987e97f68a2608e2c4628e7be32d25420
Aniekan210 <ani8dar@gmail.com>
2024-08-22 13:46:24 +0100

    nuun

--- app/about/page.js
+++ app/about/page.js
@@ -1,15 +1,33 @@
-import { useResponsiveJSX } from '@/app/useResponsiveJSX.js';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+
+const pageVariants = {
+  initial: { opacity: 0, y: -50 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+  exit: { opacity: 0, y: -50, transition: { duration: 0.3 } },
+};
 
 const page = () => {
-    const breakpoint = useResponsiveJSX([600,900,1200]);
     return (
-        <>
-            {breakpoint === 0 && <div>Mobile View</div>}
-            {breakpoint === 1 && <div>Tablet View</div>}
-            {breakpoint === 2 && <div>Desktop View</div>}
-            {breakpoint === 3 && <div>Large Desktop View</div>}
-        </>
-    )
+        <motion.div
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        variants={pageVariants}
+    >
+        <nav>
+        <Link href="/">Go to Home Page</Link>
+        </nav>
+        <h1>About Page</h1>
+        <motion.p
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1, transition: { duration: 1 } }}
+        exit={{ opacity: 0, scale: 0.8, transition: { duration: 0.5 } }}
+        >
+        This is the About Page!
+        </motion.p>
+    </motion.div>
+    );
 }
 
-export default page
+export default page;
